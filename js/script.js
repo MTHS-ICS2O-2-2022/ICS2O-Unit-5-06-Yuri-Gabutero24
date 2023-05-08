5ac1e138af4ba68083bf8a89668156e9a8c71194
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-08 17:19:27 +0000

    favicon and js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@
 // Created on: May 2023
 // This file contains the JS functions for index.html
 
+"use strict"
+
+function myButtonClicked () {
+  // this function does repeated addition
+  // input
+  let text = ""
+  let counter = 0
+  const firstNumber = parseInt(document.getElementById("first-number").value)
+  const secondNumber = parseInt(document.getElementById("second-number").value)
+
+  // process
+  while (counter < firstNumber) {
+    text = text + secondNumber + " "
+    counter = counter + 1
+  }
+
+  // output
+  document.getElementById("the-answer").innerHTML = text
+}
